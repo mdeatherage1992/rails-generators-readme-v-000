380e59968b5d0ed34dffbc743cd4c6b6fead52d8
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-08-04 13:54:40 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180804135336) do
+ActiveRecord::Schema.define(version: 20180804135430) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20180804135336) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "category_id"
+    t.boolean  "post_status"
   end
 
 end
